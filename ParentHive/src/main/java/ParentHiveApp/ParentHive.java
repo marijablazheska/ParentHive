4c6d82e26255d14473124039155bbf4e640c0ace
marijablazheska <167658497+marijablazheska@users.noreply.github.com>
2025-04-27 01:38:07 +0200

    initial app setup and templates
    set up the app layout;
    added templates and initial design to ones with repetitive style;
    added initial controller for the purpose of testing templates

---
+++ ParentHive/src/main/java/ParentHiveApp/ParentHive.java
@@ -0,0 +1,11 @@
+package ParentHiveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentHive {
+    public static void main(String[] args) {
+            SpringApplication.run(ParentHive.class, args);
+    }
+}
---
+++ ParentHive/src/main/java/ParentHiveApp/web/controller/PageController.java
@@ -0,0 +1,23 @@
+package ParentHiveApp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // will load resources/templates/home.html
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile"; // will load resources/templates/profile.html
+    }
+    @GetMapping("/help")
+    public String help() {
+        return "help"; // will load resources/templates/help.html
+    }
+}
+
